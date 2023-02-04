e5dc61412bab87a631681e84774e962112c6b5f4
Guslang <jandbond@gmail.com>
2023-02-04 10:33:44 +0900

    Update blogInfo.py

--- blogInfo.py
+++ blogInfo.py
@@ -1,4 +1,4 @@
 # tstory API 정보
-access_token = "티스토리에서 생성한 Access_Token 토큰 입력"
-blogName = "블로그 이름 입력"
-categoryId = "카테고리 ID (숫자) 입력"
+access_token = "티스토리에서 발급한 API Access-Token 입력"
+blogName = "블로그 이름(XXX) 입력 : http://XXX.tistory.com/"
+categoryId = "블로그 카테고리 번호 입력"
