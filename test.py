92df0d482712d8e459d53d738c9ac8fd81847e45
Guslang <jandbond@gmail.com>
2023-12-11 21:48:59 +0900

    Update test.py
    웹사이트 변경에 따른 소스 수정
    - URL, html5lib --> html.parser 변경

--- test.py
+++ test.py
@@ -3,18 +3,18 @@ from bs4 import BeautifulSoup
 import sys
 
 # url 정보 수집하기
-url = 'http://www.yes24.com/24/Category/BestSeller?CategoryNumber=001&sumgb=06'
+url = 'https://www.yes24.com/Product/Category/BestSeller?CategoryNumber=001&sumgb=06'
 response = requests.get(url)
 
 # status_code가 200이면 정상
 if response.status_code == 200:
     html = response.text
-    soup = BeautifulSoup(html, 'html5lib')        
+    soup = BeautifulSoup(html, 'html.parser')        
 else : 
     print(response.status_code)
     sys.exit(0)
     
 # 제목          
-bookTitles = soup.select('#category_layout > tbody > tr:nth-child(1) > td.goodsTxtInfo > p:nth-child(1) > a:nth-child(1)')
+bookTitles = soup.select('#yesBestList > li:nth-child(1) > div > div.item_info > div.info_row.info_name > a.gd_name')
 bookTitle = bookTitles[0].text  
 print(bookTitle)
